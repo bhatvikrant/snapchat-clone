77d8d5796dc576a91aa1b23ae12b23b58028bd72
Vikrant Bhat <vik.bhat223@gmail.com>
2021-05-04 00:21:18 +0530

    save image to firebase bucket and db

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,7 +1,93 @@
+import { useEffect } from "react";
 import "./Preview.css";
+import { resetCameraImage, selectCameraImage } from "../features/cameraSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+
+import {
+	AttachFile,
+	Create,
+	Crop,
+	MusicNote,
+	Note,
+	TextFields,
+	Timer,
+	Close,
+	Send,
+} from "@material-ui/icons";
+import { v4 as uuid } from "uuid";
+import { db, storage } from "../firebase";
+import firebase from "firebase";
 
 const Preview = () => {
-	return <div></div>;
+	const cameraImage = useSelector(selectCameraImage);
+
+	const history = useHistory();
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		if (!cameraImage) {
+			history.replace("/");
+		}
+	}, [cameraImage, history]);
+
+	const closePreview = () => {
+		dispatch(resetCameraImage());
+	};
+
+	const sendPost = () => {
+		const id = uuid();
+		const uploadTask = storage
+			.ref(`posts/${id}`)
+			.putString(cameraImage, "data_url");
+
+		uploadTask.on(
+			"state_changed",
+			null,
+			err => {
+				// Error function
+				console.log("err:", err);
+			},
+			() => {
+				// Complete function
+				storage
+					.ref("posts")
+					.child(id)
+					.getDownloadURL()
+					.then(url => {
+						db.collection("posts").add({
+							imageUrl: url,
+							username: "viki",
+							read: false,
+							// profileimage
+							timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+						});
+
+						history.replace("/chats");
+					});
+			},
+		);
+	};
+	return (
+		<div className="preview">
+			<Close className="preview__close" onClick={closePreview} />
+			<div className="preview__toolbarRight">
+				<TextFields />
+				<Create />
+				<Note />
+				<MusicNote />
+				<AttachFile />
+				<Crop />
+				<Timer />
+			</div>
+			<img src={cameraImage} alt="" />
+
+			<div className="preview__footer" onClick={sendPost}>
+				<h2>Send Now</h2>
+				<Send fontSize="small" className="preview__sendIcon" />
+			</div>
+		</div>
+	);
 };
 
 export default Preview;
