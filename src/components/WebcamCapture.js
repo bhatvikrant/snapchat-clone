09581acede4ed390eff45e06d18855062c3e7142
Vikrant Bhat <vik.bhat223@gmail.com>
2021-05-03 23:43:01 +0530

    basic styles

--- src/components/WebcamCapture.js
+++ src/components/WebcamCapture.js
@@ -4,6 +4,7 @@ import { useDispatch } from "react-redux";
 import RadioButtonUncheckedIcon from "@material-ui/icons/RadioButtonUnchecked";
 import { setCameraImage } from "../features/cameraSlice";
 import { useHistory } from "react-router-dom";
+import "./WebcamCapture.css";
 
 const videoConstraints = {
 	width: 250,
