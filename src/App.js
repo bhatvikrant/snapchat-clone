98cc940d7ae4ac897a61cb2a03a4938b09a249af
Vikrant Bhat <vik.bhat223@gmail.com>
2021-05-04 02:01:50 +0530

    Login implemented

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
@@ -6,28 +6,55 @@ import WebcamCapture from "./components/WebcamCapture";
 import Preview from "./components/Preview";
 import Chats from "./components/Chats";
 import ChatView from "./components/ChatView";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/appSlice";
+import Login from "./components/Login";
+import { auth } from "./firebase";
 
 function App() {
+	const dispatch = useDispatch();
+	const user = useSelector(selectUser);
+
+	useEffect(() => {
+		auth.onAuthStateChanged(authUser => {
+			if (authUser) {
+				dispatch(
+					login({
+						username: authUser.user.displayName,
+						profilePic: authUser.user.photoURL,
+						id: authUser.user.uid,
+					}),
+				);
+			} else {
+				dispatch(logout());
+			}
+		});
+	}, []);
+
 	return (
 		<div className="app">
-			<div className="app__body">
-				<Router>
-					<Switch>
-						<Route path="/chats/view">
-							<ChatView />
-						</Route>
-						<Route path="/chats">
-							<Chats />
-						</Route>
-						<Route path="/preview">
-							<Preview />
-						</Route>
-						<Route exact path="/">
-							<WebcamCapture />
-						</Route>
-					</Switch>
-				</Router>
-			</div>
+			<Router>
+				{!user ? (
+					<Login />
+				) : (
+					<div className="app__body">
+						<Switch>
+							<Route path="/chats/view">
+								<ChatView />
+							</Route>
+							<Route path="/chats">
+								<Chats />
+							</Route>
+							<Route path="/preview">
+								<Preview />
+							</Route>
+							<Route exact path="/">
+								<WebcamCapture />
+							</Route>
+						</Switch>
+					</div>
+				)}
+			</Router>
 		</div>
 	);
 }
